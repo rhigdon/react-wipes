61e94152364bc18acd83d2abf9a5efecdf4c7c20
Ryan Higdon <133530+rhigdon@users.noreply.github.com>
2021-07-06 12:11:10 -0400

    Updates

--- precache-manifest.1a00c58a900674fc49ad9ddf63c81bf4.js
+++ precache-manifest.1a00c58a900674fc49ad9ddf63c81bf4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa1435f599ba2a8141fc7e0015459ec6",
+    "revision": "d389adf106601a722f60090dffa452c6",
     "url": "/react-wipes/index.html"
   },
   {
-    "revision": "332b61fe3d346a9eb5eb",
+    "revision": "e1ac703ed5f129164f38",
     "url": "/react-wipes/static/css/main.69a1fbe0.chunk.css"
   },
   {
-    "revision": "5b9c242ad2e46f153c47",
-    "url": "/react-wipes/static/js/2.7b112e95.chunk.js"
+    "revision": "e691af4f78943bed1704",
+    "url": "/react-wipes/static/js/2.e1fc8eff.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/react-wipes/static/js/2.7b112e95.chunk.js.LICENSE.txt"
+    "url": "/react-wipes/static/js/2.e1fc8eff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "332b61fe3d346a9eb5eb",
-    "url": "/react-wipes/static/js/main.f265e1cf.chunk.js"
+    "revision": "e1ac703ed5f129164f38",
+    "url": "/react-wipes/static/js/main.0f992b38.chunk.js"
   },
   {
     "revision": "3aadb59d9e1114cafad5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-wipes/precache-manifest.b318cc1a93ad81ab99c6445b15706f8b.js"
+  "/react-wipes/precache-manifest.1a00c58a900674fc49ad9ddf63c81bf4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-wipes/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
